d3fa34ba8b440c9f18c21f4fd2b65043af1b2ccf
Zhangliang <dzl199401@126.com>
2018-12-03 21:20:49 -0800

    FIX

--- src/components/QtyInputList/fixPic/index.js
+++ src/components/QtyInputList/fixPic/index.js
@@ -1,18 +1,41 @@
 import React from "react";
 import "./style.scss";
-import {connect} from "react-redux"
-import { imagesSelector, priceAreaInfoSelector, activeVariationSelector } from "../../../selectors";
+import { connect } from "react-redux";
+import {
+  imagesSelector,
+  priceAreaInfoSelector,
+  activeVariationSelector
+} from "../../../selectors";
 
 export class fixPic extends React.Component {
   render() {
-    
+    const { variation: { filters = [], name = "" } = {} } = this.props;
+    console.log(this.props);
     return (
       <div className={"wbro-product-fixed-top"}>
-        <span style={{position:"fixed", top:"2%", left:"5%", fontSize:24}}>${parseFloat(this.props.price.price).toFixed(2)}</span>
-        <span style={{position:"fixed", top:"2%", left:"30%", fontSize:24}}>{this.props.filter !== undefined ? 
-          <span><span style={{fontWeight:500}}>{this.props.filter.filter + " "}</span><span style={{fontSize:16}}>{this.props.filter.name !== undefined ? this.props.filter.name : "" }</span></span> : ""
-        }</span>
-        <img style={{position:"fixed", maxHeight:"60px", left:"80%", marginTop:5, marginBottom:5}} src={this.props.images} />
+        <span
+          style={{ position: "fixed", top: "2%", left: "5%", fontSize: 24 }}
+        >
+          ${parseFloat(this.props.price.price).toFixed(2)}
+        </span>
+        <span
+          style={{ position: "fixed", top: "2%", left: "30%", fontSize: 24 }}
+        >
+          <span>
+            <span style={{ fontWeight: 500 }}>{filters.join(" ")}</span>
+            <span style={{ fontSize: 16, marginLeft: 16 }}>{name}</span>
+          </span>
+        </span>
+        <img
+          style={{
+            position: "fixed",
+            maxHeight: "60px",
+            left: "80%",
+            marginTop: 5,
+            marginBottom: 5
+          }}
+          src={this.props.images}
+        />
       </div>
     );
   }
@@ -21,7 +44,7 @@ export class fixPic extends React.Component {
 const mapState = state => ({
   images: imagesSelector(state),
   price: priceAreaInfoSelector(state),
-  filter: activeVariationSelector(state)
-})
+  variation: activeVariationSelector(state)
+});
 
-export default connect(mapState)(fixPic)
+export default connect(mapState)(fixPic);
